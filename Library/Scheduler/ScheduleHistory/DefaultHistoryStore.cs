e96f507163a4fdb3cd01f6f91edd60ca0bdc20fd
Philippe Leybaert <philippe@activa.be>
2013-02-13 11:55:38 +0100

    Fix threading issue in Scheduler

--- Library/Scheduler/ScheduleHistory/DefaultHistoryStore.cs
+++ Library/Scheduler/ScheduleHistory/DefaultHistoryStore.cs
@@ -5,19 +5,21 @@ namespace Vici.Core.Scheduling
 {
     public class DefaultHistoryStore : IScheduleHistoryStore
     {
+        private readonly object _lock = new object();
         private readonly Dictionary<string, DateTime> _lastRunTimes = new Dictionary<string, DateTime>();
 
         public DateTime LastRun(string taskId)
         {
             DateTime lastRun;
 
-            return _lastRunTimes.TryGetValue(taskId, out lastRun) ? lastRun : DateTime.MinValue;
+            lock (_lock)
+                return _lastRunTimes.TryGetValue(taskId, out lastRun) ? lastRun : DateTime.MinValue;
         }
 
         public void SetLastRun(string taskId, DateTime lastRun)
         {
-            _lastRunTimes[taskId] = lastRun;
-            
+            lock (_lock)
+                _lastRunTimes[taskId] = lastRun;
         }
     }
 }
--- Library/Scheduler/Scheduler.cs
+++ Library/Scheduler/Scheduler.cs
@@ -5,27 +5,23 @@ namespace Vici.Core.Scheduling
     public abstract class Scheduler
     {
         private readonly string _schedulerId;
-        private IScheduleHistoryStore _historyStore;
-        private static IScheduleHistoryStore _defaultHistoryStore = new DefaultHistoryStore();
-
         private ITimeProvider _timeProvider = new RealTimeProvider();
 
-        public static IScheduleHistoryStore DefaultHistoryStore
-        {
-            set { _defaultHistoryStore = value; }
-        }
-
-        public IScheduleHistoryStore HistoryStore
-        {
-            get { return _historyStore ?? _defaultHistoryStore; }
-            set { _historyStore = value; }
-        }
+        public static IScheduleHistoryStore DefaultHistoryStore { private get; set; }
+        public IScheduleHistoryStore HistoryStore { get; set; }
 
         public abstract bool ShouldRun();
 
         protected Scheduler(string schedulerId)
         {
             _schedulerId = schedulerId ?? Guid.NewGuid().ToString("N");
+
+            HistoryStore = DefaultHistoryStore;
+        }
+
+        static Scheduler()
+        {
+            DefaultHistoryStore = new DefaultHistoryStore();
         }
 
         public DateTime LastRun
